dae859f2cc1278df6ca8fa796a8f481f3941af06
AbnerHS <rick.santos123@hotmail.com>
2022-02-22 20:31:23 -0300

    gráfico de barras e de linha

--- processo-api/src/app.js
+++ processo-api/src/app.js
@@ -24,5 +24,17 @@ app.get('/status', async (req, res) => {
   res.send(query);
 });
 
+app.get('/solicitante', async (req, res) => {
+  const { QueryTypes } = require('sequelize');
+  const query = await sequelize.query("SELECT SOLICITANTEDEPARTAMENTO, count(CASEID) as `COUNT` FROM processos GROUP BY SOLICITANTEDEPARTAMENTO", { type: QueryTypes.SELECT });
+  res.send(query);
+});
+
+app.get('/data', async (req, res) => {
+  const { QueryTypes } = require('sequelize');
+  const query = await sequelize.query("SELECT DATE(DATAABERTURA) AS `DATA`, COUNT(CASEID) as `COUNT` FROM processos GROUP BY `DATA`", { type: QueryTypes.SELECT });
+  res.send(query);
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port);
--- processo-front/js/index.js
+++ processo-front/js/index.js
@@ -35,3 +35,60 @@ axios.get('http://localhost:3000/status')
   })
   .catch(error => console.log(error));
 
+axios.get('http://localhost:3000/solicitante')
+  .then(response => {
+    let dataset = response.data;
+    let labels = dataset.map((label) => {
+      return label.SOLICITANTEDEPARTAMENTO;
+    });
+    let data = dataset.map((data) => {
+      return data.COUNT;
+    })
+    const graph = {
+      labels: labels,
+      datasets: [{
+        label: 'Quantidade por Local',
+        backgroundColor: 'rgb(0, 100, 255)',
+        data: data,
+      }]
+    };
+    const config = {
+      type: 'bar',
+      data: graph,
+      options: {}
+    };
+    const chartBarras = new Chart(
+      document.getElementById('chartBarras'),
+      config
+    );
+  })
+  .catch(error => console.log(error));
+
+axios.get('http://localhost:3000/data')
+  .then(response => {
+    let dataset = response.data;
+    let labels = dataset.map((label) => {
+      return label.DATA;
+    });
+    let data = dataset.map((data) => {
+      return data.COUNT;
+    })
+    const graph = {
+      labels: labels,
+      datasets: [{
+        label: 'Quantidade por Data',
+        borderColor: 'rgb(0, 255, 100)',
+        data: data,
+      }]
+    };
+    const config = {
+      type: 'line',
+      data: graph,
+      options: {}
+    };
+    const chartLinhas = new Chart(
+      document.getElementById('chartLinhas'),
+      config
+    );
+  })
+  .catch(error => console.log(error));
